ecb49f74da7842b1b3fae89d066abfbae7874179
Ryan Nguyen <haiimryan@yahoo.com>
2019-01-05 15:13:53 -0800

    animation

---
+++ Assets/RyanMove.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RyanMove : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+    public Rigidbody2D rigidbody2D;
+
+    float horizontalMove = 0f;
+    float runSpeed = 40f;
+    float jumpTotal = 0;
+    bool jump = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("Speed",Mathf.Abs(horizontalMove));
+       
+        if (Input.GetButtonDown("Jump"))
+        {
+            jumpTotal += 1;
+            jump = true;
+            animator.SetBool("Jump", true);
+
+            /*
+            if(jumpTotal >= 2)
+            {
+                controller.Move(horizontalMove, false, true);
+
+            }
+            */
+        }
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("Jump", false);
+        jumpTotal = 0;
+        jump = false;
+    }
+
+    private void FixedUpdate()
+    {
+        // Moves our character
+        controller.Move(horizontalMove, false, jump);
+        //jump = false;
+    }
+}
